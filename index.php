95cef66ddcade31724747240569ea02cf715c057
Jnik2003 <kaunas16@gmail.com>
2021-01-10 19:12:38 +0300

    100121 commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>ToDo list/ Берет базу адресов из csv файла, отправляет список дел на почту, записывает лог</title>
 	<style>
 
 		.task{
